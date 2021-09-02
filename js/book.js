c7d9b0e17050117b5bf8ad79b59803e4accc4ffa
shakilmia8 <shakilmia8@gmail.com>
2021-09-02 12:03:06 +0600

    adde added

--- js/book.js
+++ js/book.js
@@ -8,21 +8,31 @@ const loadSearch = () => {
 };
 
 const displayShow = docs => {
+    console.log(docs);
+    const restultCount = document.getElementById('result-count');
+    restultCount.innerHTML = `
+    <h2>Book Count Number: ${docs.length}</h2>
+    `;
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
     docs.forEach(doc => {
         console.log(doc);
+        const restultShow = document.getElementById('result-show');
+        restultShow.innerHTML = `
+    <h2>Book Display Show Number: ${doc.length}</h2>
+    `;
         const div = document.createElement('div');
         div.classList.add('col');
         div.textContent = '';
+        const url = `https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg`;
         div.innerHTML = `
         <div class="card h-100">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="${url}" class="card-img-top" alt="">
             <div class="card-body">
                 <h4>Book Name: ${doc.title}</h4>
                 <h5>Book Author: ${doc.author_name ? doc.author_name : 'Not Available'}</h5>
                 <p>First Publish Year: ${doc.first_publish_year ? doc.first_publish_year : 'Not Available'}</p>
-                <p>Publisher: ${doc.publisher ? doc.publisher : 'Not Available'}</p>
+                <p>Publisher: ${doc.publisher}</p>
             </div>
         </div>
         `;
